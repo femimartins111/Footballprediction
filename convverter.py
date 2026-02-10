ba6896c26a3cbbaaeada724c024ac32bbed8d066
Olorunfemi Martins <martins.femi000@gmail.com>
2026-02-09 22:24:25 -0500

    Add files via upload

---
+++ convverter.py
@@ -0,0 +1,12 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect("pl_predictions.db")
+
+df = pd.read_sql("SELECT * FROM predictions", conn)
+
+df.to_excel("pl_predictions.xlsx", index=False)
+
+conn.close()
+
+print("Exported predictions to pl_predictions.xlsx")
